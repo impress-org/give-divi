475cdf30fdb4e707c1c94142242085564d86861b
alaca <ante.laca@gmail.com>
2021-01-13 10:39:27 +0100

    refactor: husky

--- src/Divi/resources/views/admin/notices/divi-inactive-error.php
+++ src/Divi/resources/views/admin/notices/divi-inactive-error.php
@@ -3,6 +3,6 @@
 <div class="notice notice-error">
 	<p>
 		<strong><?php esc_html_e( 'Activation Error:', 'give-divi' ); ?></strong>
-		<?php esc_html_e( sprintf( '%s add-on requires Divi theme to be activated.', GIVE_DIVI_ADDON_NAME ), 'give-divi' ); ?>
+		<?php sprintf( esc_html_e( '%s add-on requires Divi theme to be activated.', 'give-divi' ), GIVE_DIVI_ADDON_NAME ); ?>
 	</p>
 </div>
