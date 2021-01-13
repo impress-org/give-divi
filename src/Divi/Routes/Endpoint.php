36aa03edf1e09ce988754c6a7bf6fa5965ef02ab
alaca <ante.laca@gmail.com>
2021-01-13 17:04:24 +0100

    Initial

---
+++ src/Divi/Routes/Endpoint.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace GiveDivi\Divi\Routes;
+
+use Give\API\RestRoute;
+use WP_Error;
+
+/**
+ * Class Endpoint
+ * @package GiveDivi\Divi\Routes
+ */
+abstract class Endpoint implements RestRoute {
+
+	/**
+	 * @var string
+	 */
+	protected $endpoint;
+
+	/**
+	 * Check user permissions
+	 * @return bool|WP_Error
+	 */
+	public function permissionsCheck() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			return new WP_Error(
+				'rest_forbidden',
+				esc_html__( 'You dont have the right permissions to use Give Divi Add-on', 'give-divi' ),
+				[ 'status' => $this->authorizationStatusCode() ]
+			);
+		}
+
+		return true;
+	}
+
+	// Sets up the proper HTTP status code for authorization.
+	public function authorizationStatusCode() {
+		if ( is_user_logged_in() ) {
+			return 403;
+		}
+
+		return 401;
+	}
+}
---
+++ src/Divi/resources/js/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const CancelToken = axios.CancelToken.source();
+
+export default axios.create( {
+	baseURL: window.GiveDivi.apiRoot,
+	headers: {
+		'Content-Type': 'application/json',
+		'X-WP-Nonce': window.GiveDivi.apiNonce,
+	},
+} );
