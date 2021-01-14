4e677f4ce09206fa8f41b345275b98a4c9348879
alaca <ante.laca@gmail.com>
2021-01-14 14:48:02 +0100

    refactor: add donor wall component

--- src/Divi/resources/js/give-divi.js
+++ src/Divi/resources/js/give-divi.js
@@ -2,7 +2,8 @@
 import $ from 'jquery';
 
 import DonationForm from '../../Modules/DonationForm';
+import DonorWall from '../../Modules/DonorWall';
 
 $( window ).on( 'et_builder_api_ready', ( event, API ) => {
-	API.registerModules( [ DonationForm ] );
+	API.registerModules( [ DonationForm, DonorWall ] );
 } );
