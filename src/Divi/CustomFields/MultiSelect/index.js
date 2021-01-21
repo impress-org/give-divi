39dac56e509d521a9fa367b20f6011fad13f134b
alaca <ante.laca@gmail.com>
2021-01-21 14:30:03 +0100

    feature: multi select custom field component

---
+++ src/Divi/CustomFields/MultiSelect/index.js
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+// eslint-disable-next-line no-unused-vars
+import MultiSelect from '@khanacademy/react-multi-select';
+
+import './style.css';
+
+class SimpleInput extends Component {
+	static slug = 'give_multi_select';
+
+	options = Object.entries( this.props.fieldDefinition.options ).map( ( [ value, label ] ) => ( { label, value } ) );
+	selectedOptions = ( this.props.value.length > 0 )
+		? this.props.value.split( ',' )
+		: [];
+
+	state = {
+		selected: this.selectedOptions,
+	}
+
+	_onChange = ( selected ) => {
+		this.setState( { selected } );
+
+		const selectedOptions = selected.filter( ( option ) => option.length > 0 );
+		const filteredOptions = ( selectedOptions.length > 1 )
+			? selectedOptions.join( ',' )
+			: selectedOptions[ 0 ];
+
+		this.props._onChange( this.props.name, filteredOptions );
+	}
+
+	render() {
+		return (
+			<MultiSelect
+				options={ this.options }
+				selected={ this.state.selected }
+				onSelectedChanged={ this._onChange }
+				value={ this.props.value }
+			/>
+		);
+	}
+}
+
+export default SimpleInput;
