9b1e2ca41a69b4e1e8973fce3f0749f665c8b427
alaca <ante.laca@gmail.com>
2021-01-21 14:32:12 +0100

    feature: register custom field component

--- src/Divi/resources/js/give-divi.js
+++ src/Divi/resources/js/give-divi.js
@@ -9,7 +9,10 @@ import RegistrationForm from '../../Modules/RegistrationForm';
 import LoginForm from '../../Modules/LoginForm';
 import Totals from '../../Modules/Totals';
 import ProfileEditor from '../../Modules/ProfileEditor';
+// Custom fields
+import SimpleInput from '../../CustomFields/MultiSelect';
 
 $( window ).on( 'et_builder_api_ready', ( event, API ) => {
-	API.registerModules( [ DonationForm, DonorWall, FormGoal, DonationReceipt, RegistrationForm, LoginForm, Totals, ProfileEditor ] )
+	API.registerModules( [ DonationForm, DonorWall, FormGoal, DonationReceipt, RegistrationForm, LoginForm, Totals, ProfileEditor ] );
+	API.registerModalFields( [ SimpleInput ] );
 } );
