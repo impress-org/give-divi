e58c83df0a67a9c5ed1a0177b59dc527a7c269b8
alaca <ante.laca@gmail.com>
2021-01-25 13:00:02 +0100

    refactor: form test

--- tests/unit/tests/DonationFormTest.php
+++ tests/unit/tests/DonationFormTest.php
@@ -4,29 +4,71 @@ use PHPUnit\Framework\TestCase;
 use GiveDivi\Divi\Repositories\Forms;
 use GiveDivi\Divi\Modules\DonationForm\Module as DonationFormModule;
 
+use Give\TestData\Repositories\DonationFormRepository;
+
 final class DonationFormTest extends TestCase {
 
 	public function testShortcodeOutput() {
+
+		// Setup Donation forms
+		$donationFormRepository = give( DonationFormRepository::class );
+
+		$forms = [
+			[
+				'post_title'     => 'Legacy Form',
+				'post_name'      => 'legacy-form',
+				'post_author'    => 1,
+				'post_date'      => date( 'Y-m-d H:i:s' ),
+				'donation_goal'  => false,
+				'donation_terms' => [],
+				'form_template'  => 'legacy',
+				'random_amount'  => '10.00',
+			],
+			[
+				'post_title'     => 'Multi-step Form',
+				'post_name'      => 'multi-step-form',
+				'post_author'    => 1,
+				'post_date'      => date( 'Y-m-d H:i:s' ),
+				'donation_goal'  => false,
+				'donation_terms' => [],
+				'form_template'  => 'sequoia',
+				'random_amount'  => '20.00',
+			],
+		];
+
+		foreach ( $forms as $form ) {
+			$donationFormRepository->insertDonationForm( $form );
+		}
+
 		$module = new DonationFormModule( new Forms );
 
 		$class  = new \ReflectionClass( $module );
 		$method = $class->getMethod( 'render' );
 
-		$attributes = [
-			'id'            => 1,
-			'display_style' => 'onpage',
-			'show_title'    => true,
-			'show_goal'     => true,
-		];
+		// Get inserted donation forms
+		$donationForms = get_posts( [
+			'post_type' => 'give_forms',
+			'fields'    => 'id'
+		] );
 
-		// Generate shortcode
-		$shortcode = generate_shortcode( 'give_form', $attributes );
+		$_SERVER['QUERY_STRING'] = '';
 
-		$this->expectOutputString(
-			do_shortcode( $shortcode )
-		);
+		foreach ( $donationForms as $form ) {
+			$attributes = [
+				'id'            => $form->ID,
+				'display_style' => 'onpage',
+				'show_title'    => true,
+				'show_goal'     => true,
+			];
 
-		echo $method->invokeArgs( $module, [ $attributes, null, '' ] );
+			$shortcode = sprintf( '[give_form id="%s" display_style="onpage" show_title="true" show_goal="true"]', $form->ID );
+
+			$this->assertEquals(
+				do_shortcode( $shortcode ),
+				$method->invokeArgs( $module, [ $attributes, null, '' ] )
+			);
+
+		}
 	}
 
 }
