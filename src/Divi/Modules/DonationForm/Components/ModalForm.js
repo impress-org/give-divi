47415d2310c3495d85c033395bcb50a55d502f2f
Ante Laca <ante.laca@get10up.com>
2023-12-11 12:33:36 +0100

    refactor: use this.props

--- src/Divi/Modules/DonationForm/Components/ModalForm.js
+++ src/Divi/Modules/DonationForm/Components/ModalForm.js
@@ -44,7 +44,7 @@ export default class ModalForm extends React.Component {
                                     <path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path>
                                 </svg>
                             </button>
-                            <Iframe dataSrc={dataSrc} />
+                            <Iframe dataSrc={this.props.dataSrc} />
                         </dialog>,
                         document.body
                     )}
